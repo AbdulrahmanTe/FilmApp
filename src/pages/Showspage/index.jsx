eb5aeb1c5e5b4ed8acf145b8d65b92de65174d64
Abdulrahman <abdulx9999@gmail.com>
2023-12-19 16:25:59 +0000

    mall

--- src/pages/Showspage/index.jsx
+++ src/pages/Showspage/index.jsx
@@ -5,7 +5,7 @@ import { ShowGallery } from '../../components'
 export default function ShowsPage() {
   return (
     <>
-        <h1>Shows Page</h1>
+        <h1>Shows Pages</h1>
         <ShowGallery></ShowGallery>
 
     </>
